687de4c6e5c3aa3ed5b6f2a92b86b0ad6b138b93
MrJake222 <norbertzpilicy@gmail.com>
2023-04-07 13:26:19 +0200

    cleanup

---
+++ libs/waveheader.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "ff.h"
+
+typedef unsigned int uint;
+
+struct wave_header {
+    char riff_str[4];
+    uint file_size;
+    char wave_str[4];
+    char fmt_str[4];
+    uint format_size;
+    unsigned short format_type;
+    unsigned short channels;
+    uint sample_rate;
+    uint bytes_per_second;
+    unsigned short bytes_per_sample;
+    unsigned short bits_per_sample;
+    char data_str[4];
+    uint data_size;
+};
+
+#define WAVE_HEADER_SIZE sizeof(struct wave_header)
+
+class WaveHeader {
+
+    struct wave_header hdr;
+    uint duration_sec;
+
+public:
+    FRESULT read(FIL* fp, uint* read);
+
+    uint get_data_size() { return hdr.data_size; }
+    uint get_duration() { return duration_sec; }
+    uint byte_to_sec(uint byte) { return byte / hdr.bytes_per_second; }
+
+    int check();
+    void print();
+};
--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ uint16_t ls, rs;
 #include "f_util.h"
 #include "ff.h"
 #include "hw_config.h"
+#include "libs/waveheader.hpp"
 
 void pwm_fast_wrap_irq() {
     // static bool led = false;
@@ -102,7 +103,10 @@ uint16_t scale_and_center(uint16_t sample) {
     return (uint16_t) sig;
 }
 
-volatile int aidx = 0;
+volatile int aread = 0; // non-wrapping index
+volatile int aidx = 0;  // wrapping index
+
+WaveHeader hdr;
 
 void pwm_audio_wrap_irq() {
 
@@ -116,9 +120,15 @@ void pwm_audio_wrap_irq() {
 
     pwm_set_both_levels(PWM_AUDIO_SLICE, ls, rs);
 
+    aread+=4;
     aidx+=4;
     if (aidx == BUF_SIZE) {
         aidx = 0;
+    }
+
+    if (aread == hdr.get_data_size()) {
+        // stop pwm
+        pwm_set_enabled(PWM_AUDIO_SLICE, false);
     }
 }
 
@@ -130,7 +140,7 @@ void pwm_wrap_irq() {
         pwm_fast_wrap_irq();
 }
 
-void init_pwm(uint slice, pwm_config* cfg, bool irq_en) {
+void init_pwm(uint slice, pwm_config* cfg, bool irq_en, bool start) {
     uint pinA = (slice << 1);
     uint pinB = (slice << 1) + 1;
 
@@ -139,50 +149,11 @@ void init_pwm(uint slice, pwm_config* cfg, bool irq_en) {
 
     pwm_set_irq_enabled(slice, irq_en);
 
-    pwm_init(slice, cfg, true);
+    pwm_init(slice, cfg, start);
 }
 
-struct wave_header {
-    char riff_str[4];
-    uint file_size;
-    char wave_str[4];
-    char fmt_str[4];
-    uint format_size;
-    unsigned short format_type;
-    unsigned short channels;
-    uint sample_rate;
-    uint bytes_per_second;
-    unsigned short bytes_per_sample;
-    unsigned short bits_per_sample;
-    char data_str[4];
-    uint data_size;
-};
-
-#define WAVE_HEADER_SIZE sizeof(struct wave_header)
-
-void print_wave_header(struct wave_header* hdr) {
-    printf("file size:   %u\n", hdr->file_size);
-    printf("channels:    %u\n", hdr->channels);
-    printf("sample rate: %u\n", hdr->sample_rate);
-    printf("bitrate:     %u\n", hdr->bytes_per_second * 8);
-    printf("bit depth:   %u\n", hdr->bits_per_sample);
-    printf("data size:   %u\n", hdr->data_size);
-}
-
-int load_exact_into(FIL* fil, uint8_t* buf, uint buf_size) {
-    uint read = 0;
-    while (read < buf_size) {
-        uint read_now;
-        FRESULT fr = f_read(fil, buf + read, buf_size - read, &read_now);
-        if (fr != FR_OK) {
-            panic("f_read error: %s (%d)\n", FRESULT_str(fr), fr);
-            return 1;
-        }
-
-        read += read_now;
-    }
-
-    return 0;
+void fs_err(FRESULT fr, const char* tag) {
+    panic("%s: %s (id=%d)\n", tag, FRESULT_str(fr), fr);
 }
 
 int main() {
@@ -242,7 +213,7 @@ int main() {
         err;
     #endif
 
-    // init_pwm(PWM_AUDIO_SLICE, &cfg, true);
+    init_pwm(PWM_AUDIO_SLICE, &cfg, true, false);
 
     irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_wrap_irq);
     irq_set_enabled(PWM_IRQ_WRAP, true);
@@ -260,71 +231,92 @@ int main() {
 
     sleep_ms(2000);
 
+    FRESULT fr;
+    FIL fp;
+
     sd_card_t *pSD = sd_get_by_num(0);
-    FRESULT fr = f_mount(&pSD->fatfs, pSD->pcName, 1);
-    if (fr != FR_OK)
-        panic("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
+
+    fr = f_mount(&pSD->fatfs, pSD->pcName, 1);
+    if (fr != FR_OK) {
+        fs_err(fr, "f_mount");
+    }
 
     puts("\n");
 
-    FIL fil;
-//    fr = f_open(&fil, "The Memory Remains.wav", FA_READ);
-//    fr = f_open(&fil, "Rock or Bust/01. Rock Or Bust.wav", FA_READ);
-    fr = f_open(&fil, "4mmc.wav", FA_READ);
+//    const char* path = "The Memory Remains.wav";
+//    const char* path = "Rock or Bust/01. Rock Or Bust.wav";
+//    const char* path = "4mmc.wav";
+    const char* path = "too_much_short.wav";
+
+    printf("opening: %s\n", path);
+
+    fr = f_open(&fp, path, FA_READ);
     if (fr != FR_OK) {
-        panic("f_open error: %s (%d)\n", FRESULT_str(fr), fr);
+        fs_err(fr, "f_open");
     }
 
-    printf("header size: %u\n", WAVE_HEADER_SIZE);
+    uint read;
 
-    struct wave_header hdr;
-    load_exact_into(&fil, (uint8_t*)&hdr, WAVE_HEADER_SIZE);
+    fr = hdr.read(&fp, &read);
+    if (fr != FR_OK) {
+        fs_err(fr, "f_read header");
+    }
 
-    print_wave_header(&hdr);
+    printf("header size: %u, read %u\n", WAVE_HEADER_SIZE, read);
+
+    if (hdr.check()) {
+        panic("wrong header\n");
+    }
+
+    hdr.print();
 
     // preload buffer
-    load_exact_into(&fil, (uint8_t*)audio, BUF_SIZE);
-
-    for (int off=0; off<20; off++) {
-        for (int i = 0; i < 17; i++) {
-            printf("%02x ", audio[off * 17 + i]);
-        }
-        puts("");
+    fr = f_read(&fp, (uint8_t*)audio, BUF_SIZE, &read);
+    if (fr != FR_OK) {
+        fs_err(fr, "f_read preload");
     }
 
     // start pwm
-    init_pwm(PWM_AUDIO_SLICE, &cfg, true);
+    pwm_set_enabled(PWM_AUDIO_SLICE, true);
 
     int start = 0;
-    uint absolute_start = 0;
     uint last_seconds = 0;
+    bool eof = false;
 
-    while (absolute_start < hdr.data_size) {
+    while (!eof) {
         uint left = aidx > start
                 ? BUF_SIZE - (aidx - start)
                 : start - aidx;
 
         if (left < BUF_SIZE / 2) {
-            load_exact_into(&fil, audio + start, BUF_SIZE/2);
+            fr = f_read(&fp, ((uint8_t*)audio) + start, BUF_SIZE / 2, &read);
+            if (fr != FR_OK) {
+                fs_err(fr, "f_read loop");
+            }
+            if (read < BUF_SIZE/2) {
+                // EOF reached
+                eof = true;
+            }
+
             start += BUF_SIZE/2;
             if (start == BUF_SIZE) start = 0;
-
-            absolute_start += BUF_SIZE/2;
         }
 
-        uint seconds = absolute_start*8 / (hdr.channels * hdr.bits_per_sample * hdr.sample_rate);
-        uint whole_seconds = hdr.data_size*8 / (hdr.channels * hdr.bits_per_sample * hdr.sample_rate);
+        uint seconds = hdr.byte_to_sec(aread);
 
         if (seconds != last_seconds) {
             printf("%02d:%02d / %02d:%02d\r",
                    seconds/60, seconds%60,
-                   whole_seconds/60, whole_seconds%60);
+                   hdr.get_duration()/60, hdr.get_duration()%60);
 
             last_seconds = seconds;
         }
     }
 
+    while (aread < hdr.get_data_size());
+
     printf("\nfinished.\n");
+    printf("\nplayed %u / %u bytes.\n", aread, hdr.get_data_size());
 
     while (1);
 }
