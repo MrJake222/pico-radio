b338e00fb9af051c38249aca15b96143843a33c8
MrJake222 <norbertzpilicy@gmail.com>
2023-09-14 15:34:13 +0200

    Reduced card detect bouncing

--- libs/sd/sd.cpp
+++ libs/sd/sd.cpp
@@ -12,23 +12,25 @@
 #include <f_util.h>
 #include <diskio.h>
 
+#include <pico/time.h>
+
 namespace sd {
 
 static TaskHandle_t sd_task_h;
-static volatile bool card_present;
-static volatile bool card_mounted;
+static bool card_present;
+static bool card_mounted;
+static int last_interrupt_time_us;
 
 void sd_cd_callback(uint gpio, uint32_t events) {
-    if (events & GPIO_IRQ_EDGE_FALL) {
-        // card inserted
-        card_present = true;
-    }
-    else if (events & GPIO_IRQ_EDGE_RISE) {
-        // card removed
-        card_present = false;
-    }
+    if (gpio != SD_CD)
+        return;
 
+    last_interrupt_time_us = (int) time_us_32();
     vTaskNotifyGiveFromISR(sd_task_h, nullptr);
+}
+
+static inline uint32_t time_since_last_interrupt_ms() {
+    return ((int)time_us_32() - last_interrupt_time_us) / 1000u;
 }
 
 static inline void fs_err(FRESULT fr, const char* tag) {
@@ -69,15 +71,8 @@ static int unmount(sd_card_t* pSD) {
         gpio_set_input_hysteresis_enabled(gpio, true); // schmitt trigger
     }
 
-    // wait a bit for gpio to settle after boot
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-
-    // check if card is present on boot
-    // card present if gpio low
-    card_present = gpio_get(SD_CD) == 0;
     card_mounted = false;
-
-    printf("sd: detection init ok, on-boot: %s\n", card_present ? "PRESENT" : "MISSING");
+    last_interrupt_time_us = 0;
 
     // enable interrupts
     for (int gpio : all) {
@@ -87,16 +82,35 @@ static int unmount(sd_card_t* pSD) {
                 true);
     }
 
+    printf("sd: detection init ok\n");
+
     sd_card_t* pSD = sd_get_by_num(0);
 
     while (true) {
-        if (card_present) {
+
+        printf("\tsd: int %7lu ms ago\n", time_since_last_interrupt_ms());
+
+        while (time_since_last_interrupt_ms() < SD_CD_DEBOUNCE_MS)
+            // bouncing
+            // wait a bit for card to securely be inserted / gpio pull-up to settle
+            vTaskDelay((SD_CD_DEBOUNCE_MS + 100) / portTICK_PERIOD_MS);
+
+        printf("\tsd: int %7lu ms ago\n", time_since_last_interrupt_ms());
+
+        // clear any pending interrupts (0 wait time)
+        ulTaskNotifyTake(true, 0);
+
+        // check SD status (present if shorted to ground)
+        card_present = gpio_get(SD_CD) == 0;
+
+        if (card_present && !card_mounted) {
             puts("mounting sd card");
             int r = mount(pSD);
             if (r == 0)
                 card_mounted = true;
         }
-        else if (card_mounted) { // avoid unmounting if never present (on boot)
+
+        if (!card_present && card_mounted) {
             puts("unmounting sd card");
 
             // as early as possible because card was already removed
@@ -111,9 +125,6 @@ static int unmount(sd_card_t* pSD) {
         // wait for changes
         // (check at least once before waiting)
         ulTaskNotifyTake(true, portMAX_DELAY);
-
-        // wait a bit for card to securely be inserted
-        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
 
